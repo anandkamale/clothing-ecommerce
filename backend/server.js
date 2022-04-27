9fc14c2b2cc82bcbb3ec0a9f1004660ec92ff6d4
anandkamale <anand.k@codaemonsoftwares.com>
2022-04-27 11:01:23 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,10 @@ app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
+app.get('/', (req,res)=>{
+  res.send('Hello');
+});
+
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
